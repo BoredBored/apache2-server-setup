12e87931aea7b7943080bfb4bf9705a941944d50
Alexios Angeletakis <alexiosangeletakis@yahoo.com>
2017-05-13 12:06:10 -0400

    Update mksubdo.sh

--- mksubdo.sh
+++ mksubdo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 subDomain=$1
-mkdir -p "$subDomain/main/public-html"
-echo "created $subDomain/main/public-html in /sub/"
-mkdir -p "$subDomain/mobile/public-html"
-echo "created $subDomain/mobile/public-html in /sub/"
+mkdir -p "$subDomain/rootDomain/main/public-html"
+echo "created $subDomain/rootDomain/main/public-html in /sub/"
+mkdir -p "$subDomain/rootDomain/mobile/public-html"
+echo "created $subDomain/rootDomain/mobile/public-html in /sub/"
 mkdir -p "$domainName/sub"
 echo "created $subDomain/sub in /sub/"
 echo "Done. :)"
