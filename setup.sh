d8bd85f8d9e5809b29a63a498c4534f56db91713
Alexios Angeletakis <alexiosangeletakis@yahoo.com>
2017-05-12 22:17:15 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 apt-get install software-properties-common python-software-properties
+add-apt-repository ppa:git-core/ppa
+apt-get update
+apt-get install git
+apt-get update
+sudo apt-get install curl php7.0-cli git
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+composer require defuse/php-encryption
+apt-get install iptables
