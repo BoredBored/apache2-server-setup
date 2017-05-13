c71c6423d64abdcfcbd9313e8888ebaad50ae9db
Alexios Angeletakis <alexiosangeletakis@yahoo.com>
2017-05-12 22:30:07 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,10 @@ add-apt-repository ppa:git-core/ppa
 apt-get update
 apt-get install git
 apt-get update
-sudo apt-get install curl php7.0-cli git
+apt-get install curl php7.0-cli git
+add-apt-repository ppa:certbot/certbot
+apt-get update
+apt-get install python-certbot-apache
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 composer require defuse/php-encryption
 apt-get install iptables
