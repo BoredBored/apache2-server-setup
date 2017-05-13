46b18140623c6219cc0f59a5c2d0de20aa89dd89
Alexios Angeletakis <alexiosangeletakis@yahoo.com>
2017-05-13 11:56:04 -0400

    Update mkdo.sh

--- mkdo.sh
+++ mkdo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 domainName=$1
-echo "mkdir -p domainName/rootDomain/mobile/public-html"
+echo "mkdir -p domains/domainName/rootDomain/mobile/public-html"
 mkdir -p "domains/$domainName/rootDomain/mobile/public-html"
-echo "mkdir -p domainName/rootDomain/main/public-html"
+echo "mkdir -p domains/domainName/rootDomain/main/public-html"
 mkdir -p "domains/$domains/domainName/rootDomain/main/public-html"
-echo "mkdir -p domainName/sub"
+echo "mkdir -p domains/domainName/sub"
 mkdir -p "domains/$domainName/sub"
-cp "/var/www/admin/mksubdo.sh" "$domainName/sub"
+cp "/var/www/admin/mksubdo.sh" "domains/$domainName/sub"
